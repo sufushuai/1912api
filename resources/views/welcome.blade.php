9a95ac4301dc31e3cd54331e055665644da4ee10
liqin <2868838052@qq.com>
2020-09-14 11:45:50 +0800

    api

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    hhhhh
                 </div>
 
                 <div class="links">
