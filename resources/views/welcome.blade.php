8d50547d55163094a87dee57755d42dd8b56e067
liqin <2868838052@qq.com>
2020-09-14 11:48:36 +0800

    api

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    hhhhh
+                    halou
                 </div>
 
                 <div class="links">
