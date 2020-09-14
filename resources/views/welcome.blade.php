994b3e2f778ae24d1b488e8257fe034a7e548cec
2676325779@qq.com <2676325779@qq.com>
2020-09-14 09:51:14 +0800

    su

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    苏富帅
                 </div>
 
                 <div class="links">
